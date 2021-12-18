85bdcf62802ad2263c4b7a5819d65deaf4c62986
danny <dcfran@gmail.com>
2021-12-19 07:00:07 +0800

    got weather app to work. will clean up later

--- angular-weather-app/src/app/weather/weather.component.ts
+++ angular-weather-app/src/app/weather/weather.component.ts
@@ -11,7 +11,92 @@ declare const Liferay: any;
     "/o/angular-weather-app/app/weather.component.html",
 })
 export class WeatherComponent implements OnInit {
+  lat: any;
+  lng: any;
+  WeatherData: any;
+  appid: String;
+
   ngOnInit(): void {
-    console.log("WeatherComponent.ngOnInit");
+    this.appid = Liferay.Language.get("openweathermap-api-key");
+    this.WeatherData = {
+      main: {},
+      isDay: false,
+    };
+    this.getLocation();
+  }
+  onSave(event: any) {
+    console.log("WeatherComponent.onsave");
+  }
+  getWeather() {
+    console.log("getWeather", this.lat, this.lng);
+    let url =
+      "https://api.openweathermap.org/data/2.5/weather?q=Pasig" +
+      "&appid=" +
+      this.appid;
+    if (this.lat && this.lng) {
+      url =
+        "https://api.openweathermap.org/data/2.5/weather?lat=" +
+        this.lat +
+        "&lon=" +
+        this.lng +
+        "&appid=" +
+        this.appid;
+    }
+    console.log(url);
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => {
+        this.setWeatherData(data);
+      });
+  }
+  setWeatherData(data: any) {
+    this.WeatherData = data;
+    let sunsetTime = new Date(this.WeatherData.sys.sunset * 1000);
+    let sunriseTime = new Date(this.WeatherData.sys.sunrise * 1000);
+    this.WeatherData.sunset_time = sunsetTime.toLocaleTimeString();
+    let currentDate = new Date();
+    this.WeatherData.isDay =
+      currentDate.getTime() > sunriseTime.getTime() &&
+      currentDate.getTime() < sunsetTime.getTime();
+    this.WeatherData.temp_celcius = (
+      this.WeatherData.main.temp - 273.15
+    ).toFixed(0);
+    this.WeatherData.temp_min = (
+      this.WeatherData.main.temp_min - 273.15
+    ).toFixed(0);
+    this.WeatherData.temp_max = (
+      this.WeatherData.main.temp_max - 273.15
+    ).toFixed(0);
+    this.WeatherData.temp_feels_like = (
+      this.WeatherData.main.feels_like - 273.15
+    ).toFixed(0);
+    console.log(this.WeatherData);
+  }
+  getLocation() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(
+        (position: any) => {
+          if (position) {
+            console.log(
+              "Latitude: " +
+                position.coords.latitude +
+                "Longitude: " +
+                position.coords.longitude
+            );
+            this.lat = position.coords.latitude;
+            this.lng = position.coords.longitude;
+            console.log(this.lat);
+            console.log(this.lng);
+            this.getWeather();
+          }
+        },
+        (error: any) => {
+          console.log(error);
+          this.getWeather();
+        }
+      );
+    } else {
+      alert("Geolocation is not supported by this browser.");
+    }
   }
 }
