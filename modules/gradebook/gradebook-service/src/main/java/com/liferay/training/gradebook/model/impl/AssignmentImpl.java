fb9fb56f6199d1592c5ea31b2c24edd347a6c295
danny <dcfran@gmail.com>
2021-12-20 01:10:06 +0800

    regenarate grabook

--- modules/gradebook/gradebook-service/src/main/java/com/liferay/training/gradebook/model/impl/AssignmentImpl.java
+++ modules/gradebook/gradebook-service/src/main/java/com/liferay/training/gradebook/model/impl/AssignmentImpl.java
@@ -15,7 +15,7 @@
 package com.liferay.training.gradebook.model.impl;
 
 /**
- * The extended model implementation for the Assignment service. Represents a row in the &quot;Assignment_Assignment&quot; database table, with each column mapped to a property of this class.
+ * The extended model implementation for the Assignment service. Represents a row in the &quot;Gradebook_Assignment&quot; database table, with each column mapped to a property of this class.
  *
  * <p>
  * Helper methods and all application logic should be put in this class. Whenever methods are added, rerun ServiceBuilder to copy their definitions into the <code>com.liferay.training.gradebook.model.Assignment</code> interface.
