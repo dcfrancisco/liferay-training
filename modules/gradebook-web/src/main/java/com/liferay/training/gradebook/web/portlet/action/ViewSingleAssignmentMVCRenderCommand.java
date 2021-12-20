bab0dbdc7af039e235fdc70ad805ec717c473ccc
danny <dcfran@gmail.com>
2021-12-20 22:56:07 +0800

    Added Clay Toolbar

--- modules/gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/action/ViewSingleAssignmentMVCRenderCommand.java
+++ modules/gradebook-web/src/main/java/com/liferay/training/gradebook/web/portlet/action/ViewSingleAssignmentMVCRenderCommand.java
@@ -1,94 +1,76 @@
 package com.liferay.training.gradebook.web.portlet.action;
 
- import com.liferay.portal.kernel.exception.PortalException;
- import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
- import com.liferay.portal.kernel.service.UserLocalService;
- import com.liferay.portal.kernel.theme.PortletDisplay;
- import com.liferay.portal.kernel.theme.ThemeDisplay;
- import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
- import com.liferay.portal.kernel.util.ParamUtil;
- import com.liferay.portal.kernel.util.Portal;
- import com.liferay.portal.kernel.util.WebKeys;
- import com.liferay.training.gradebook.model.Assignment;
- import com.liferay.training.gradebook.service.AssignmentService;
- import com.liferay.training.gradebook.web.constants.GradebookPortletKeys;
- import com.liferay.training.gradebook.web.constants.MVCCommandNames;
+import com.liferay.portal.kernel.exception.PortalException;
+import com.liferay.portal.kernel.portlet.bridges.mvc.MVCRenderCommand;
+import com.liferay.portal.kernel.service.UserLocalService;
+import com.liferay.portal.kernel.theme.PortletDisplay;
+import com.liferay.portal.kernel.theme.ThemeDisplay;
+import com.liferay.portal.kernel.util.DateFormatFactoryUtil;
+import com.liferay.portal.kernel.util.ParamUtil;
+import com.liferay.portal.kernel.util.Portal;
+import com.liferay.portal.kernel.util.WebKeys;
+import com.liferay.training.gradebook.model.Assignment;
+import com.liferay.training.gradebook.service.AssignmentService;
+import com.liferay.training.gradebook.web.constants.GradebookPortletKeys;
+import com.liferay.training.gradebook.web.constants.MVCCommandNames;
 
- import java.text.DateFormat;
+import java.text.DateFormat;
 
- import javax.portlet.PortletException;
- import javax.portlet.RenderRequest;
- import javax.portlet.RenderResponse;
+import javax.portlet.PortletException;
+import javax.portlet.RenderRequest;
+import javax.portlet.RenderResponse;
 
- import org.osgi.service.component.annotations.Component;
- import org.osgi.service.component.annotations.Reference;
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
 
- /**
-  * MVC Command for showing the assignment submissions list view.
-  * 
-  * @author liferay
-  */
- @Component(
-     immediate = true,
-     property = {
-         "javax.portlet.name=" + GradebookPortletKeys.GRADEBOOK,
-         "mvc.command.name=" + MVCCommandNames.VIEW_ASSIGNMENT
-     },
-     service = MVCRenderCommand.class
- )
- public class ViewSingleAssignmentMVCRenderCommand implements MVCRenderCommand {
+/**
+ * MVC Command for showing the assignment submissions list view.
+ * 
+ * @author liferay
+ */
+@Component(immediate = true, property = { "javax.portlet.name=" + GradebookPortletKeys.GRADEBOOK,
+		"mvc.command.name=" + MVCCommandNames.VIEW_ASSIGNMENT }, service = MVCRenderCommand.class)
+public class ViewSingleAssignmentMVCRenderCommand implements MVCRenderCommand {
 
-     @Override
-     public String render(
-         RenderRequest renderRequest, RenderResponse renderResponse)
-         throws PortletException {
+	@Override
+	public String render(RenderRequest renderRequest, RenderResponse renderResponse) throws PortletException {
 
-         ThemeDisplay themeDisplay =
-             (ThemeDisplay) renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
+		ThemeDisplay themeDisplay = (ThemeDisplay) renderRequest.getAttribute(WebKeys.THEME_DISPLAY);
+		long assignmentId = ParamUtil.getLong(renderRequest, "assignmentId", 0);
 
-         long assignmentId = ParamUtil.getLong(renderRequest, "assignmentId", 0);
+		try {
 
-         try {
+			// Call the service to get the assignment.
 
-             // Call the service to get the assignment.
+			Assignment assignment = _assignmentService.getAssignment(assignmentId);
 
-             Assignment assignment =
-                 _assignmentService.getAssignment(assignmentId);
+			DateFormat dateFormat = DateFormatFactoryUtil.getSimpleDateFormat("EEEEE, MMMMM dd, yyyy",
+					renderRequest.getLocale());
 
-             DateFormat dateFormat = DateFormatFactoryUtil.getSimpleDateFormat(
-                 "EEEEE, MMMMM dd, yyyy", renderRequest.getLocale());
+			// Set attributes to the request.
+			renderRequest.setAttribute("assignment", assignment);
+			renderRequest.setAttribute("dueDate", dateFormat.format(assignment.getDueDate()));
+			renderRequest.setAttribute("createDate", dateFormat.format(assignment.getCreateDate()));
 
-             // Set attributes to the request.
+			// Set back icon visible.
+			PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+			String redirect = renderRequest.getParameter("redirect");
+			portletDisplay.setShowBackIcon(true);
+			portletDisplay.setURLBack(redirect);
 
-             renderRequest.setAttribute("assignment", assignment);
-             renderRequest.setAttribute(
-                 "dueDate", dateFormat.format(assignment.getDueDate()));
-             renderRequest.setAttribute(
-                 "createDate", dateFormat.format(assignment.getCreateDate()));
+			return "/assignment/view_assignment.jsp";
 
-             // Set back icon visible.
+		} catch (PortalException pe) {
+			throw new PortletException(pe);
+		}
+	}
 
-             PortletDisplay portletDisplay = themeDisplay.getPortletDisplay();
+	@Reference
+	private AssignmentService _assignmentService;
 
-             String redirect = renderRequest.getParameter("redirect");
+	@Reference
+	private Portal _portal;
 
-             portletDisplay.setShowBackIcon(true);
-             portletDisplay.setURLBack(redirect);
-
-             return "/assignment/view_assignment.jsp";
-
-         }
-         catch (PortalException pe) {
-             throw new PortletException(pe);
-         }
-     }
-
-     @Reference
-     private AssignmentService _assignmentService;
-
-     @Reference
-     private Portal _portal;
-
-     @Reference
-     private UserLocalService _userLocalService;
- }
+	@Reference
+	private UserLocalService _userLocalService;
+}
--- modules/gradebook-web/src/main/resources/META-INF/resources/view.jsp
+++ modules/gradebook-web/src/main/resources/META-INF/resources/view.jsp
@@ -1,39 +1,43 @@
-<%@ include file="/init.jsp" %>
+<%@ include file="./init.jsp" %>
+
 
 <div class="container-fluid-1280">
 
-<h1><liferay-ui:message key="Assignments" /></h1>
+    <h1><liferay-ui:message key="assignments" /></h1>
 
-<%-- Clay management toolbar. --%>
-<clay:management-toolbar
-disabled="${assignmentCount eq 0}"
-displayContext="${assignmentsManagementToolbarDisplayContext}"
-itemsTotal="${assignmentCount}"
-searchContainerId="assignmentEntries"
-selectable="false"
-/>
+    <%-- Clay management toolbar. --%>
 
-<%-- Search container. --%>
-<liferay-ui:search-container
-emptyResultsMessage="no-assignments"
-id="assignmentEntries"
-iteratorURL="${portletURL}"
-total="${assignmentCount}">
+    <clay:management-toolbar
+        disabled="${assignmentCount eq 0}"
+        displayContext="${assignmentsManagementToolbarDisplayContext}"
+        itemsTotal="${assignmentCount}"
+        searchContainerId="assignmentEntries"
+        selectable="false"
+    />
 
-<liferay-ui:search-container-results results="${assignments}" />
+    <%-- Search container. --%>
 
-<liferay-ui:search-container-row
-className="com.liferay.training.gradebook.model.Assignment"
-modelVar="entry">
+    <liferay-ui:search-container 
+        emptyResultsMessage="no-assignments"
+        id="assignmentEntries"
+        iteratorURL="${portletURL}" 
+        total="${assignmentCount}">
 
-<%@ include file="/assignment/entry_search_columns.jspf" %>
+        <liferay-ui:search-container-results results="${assignments}" />
 
-</liferay-ui:search-container-row>
+        <liferay-ui:search-container-row
+            className="com.liferay.training.gradebook.model.Assignment"
+            modelVar="entry">
 
-<%-- Iterator / Paging --%>
-<liferay-ui:search-iterator
-displayStyle="${assignmentsManagementToolbarDisplayContext.getDisplayStyle()}"
-markupView="lexicon"
-/>
-</liferay-ui:search-container>
+            <%@ include file="/assignment/entry_search_columns.jspf" %>
+
+        </liferay-ui:search-container-row>
+
+        <%-- Iterator / Paging --%>
+
+        <liferay-ui:search-iterator 
+            displayStyle="${assignmentsManagementToolbarDisplayContext.getDisplayStyle()}"
+            markupView="lexicon" 
+        />
+    </liferay-ui:search-container>
 </div>
