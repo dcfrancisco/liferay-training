9fa123209de0dea5a636bb2de47bdf8fc00663f3
--get <--get>
2025-12-28 23:04:49 +0800

    Use jakarta.servlet.HttpServletRequest in AssignmentsManagementToolbarDisplayContext

--- modules/gradebook/gradebook-web/src/main/java/com/liferay/training/gradebook/web/display/context/AssignmentsManagementToolbarDisplayContext.java
+++ modules/gradebook/gradebook-web/src/main/java/com/liferay/training/gradebook/web/display/context/AssignmentsManagementToolbarDisplayContext.java
@@ -24,7 +24,7 @@ import java.util.List;
 
 import javax.portlet.PortletException;
 import javax.portlet.PortletURL;
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletRequest;
 
 /**
  * Assigments management toolbar display context.
